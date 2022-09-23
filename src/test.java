ced3c1e42e323cbb6c3ee3d4dc3ed8704dedbea9
iAteYourCouch <sekaramatic@tsn.at>
2022-09-23 09:27:24 +0200

    hiiii

--- src/test.java
+++ src/test.java
@@ -1,6 +1,8 @@
 public class test {
     public static void main(String[] args) {
+
         System.out.println("first github project");
+        System.out.println("hallo, bitte funktioniere");
     }
 }
 
